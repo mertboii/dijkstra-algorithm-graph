a3ec19c790c9e058c6c6fc90770ca6592fde49d9
Mert Ali <mertalikurtaran3000@gmail.com>
2024-03-10 23:28:38 +0300

    Add project files.

---
+++ ConsoleApp3/Node.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Node
+    {
+        public string Name { get; set; }
+
+        public Dictionary<Node, int> Edges { get; set; }
+
+        public Node(string name) { Name = name; }
+
+        public Node(string name, Dictionary<Node, int> edges)
+        {
+            Name = name;
+            Edges = edges;
+        }
+
+        public override string ToString() => $"Node: {Name}, Edges: {Edges.Count}";
+
+        public Node AddEdge(Node node, int distance)
+        {
+            if (Edges == null)
+            {
+                Edges = new Dictionary<Node, int>();
+            }
+
+            Edges.Add(node, distance);
+            return this;
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleApp3;
+
+var nodes = new Dictionary<string, Node>
+{
+    ["A"] = new Node("A"),
+    ["B"] = new Node("B"),
+    ["C"] = new Node("C"),
+    ["D"] = new Node("D"),
+
+
+};
+
+nodes["A"].AddEdge(nodes["D"], 1).AddEdge(nodes["B"], 3).AddEdge(nodes["C"], 3);
+nodes["B"].AddEdge(nodes["A"], 3).AddEdge(nodes["D"], 1).AddEdge(nodes["C"], 3);
+nodes["C"].AddEdge(nodes["B"], 3).AddEdge(nodes["D"], 1).AddEdge(nodes["A"], 3);
+nodes["D"].AddEdge(nodes["A"], 1).AddEdge(nodes["B"], 1).AddEdge(nodes["C"], 1);
+
+
+var finalNode = nodes["C"];
+
+
+
+var distances = nodes.ToDictionary(kvp => kvp.Value, kvp => (int?)int.MaxValue);
+var parents = new Dictionary<Node, Node>();
+var undiscoveredNodes = new HashSet<Node>(nodes.Values);
+
+distances[nodes["A"]] = 0;
+
+while (undiscoveredNodes.Count > 0)
+{
+    var current = undiscoveredNodes.MinBy(node => distances[node]); undiscoveredNodes.Remove(current);
+    if (current == finalNode)
+        break;
+    foreach (var (adjacentNode, distance) in current.Edges)
+    {
+        var subDistance = distances[current] + distance;
+        if (subDistance < distances[adjacentNode])
+        {
+            distances[adjacentNode] = subDistance;
+            parents[adjacentNode] = current;
+        }
+    }
+}
+
+
+var pathNodes = new List<Node>();
+var currentNode = finalNode;
+
+while (currentNode is not null)
+{
+    pathNodes.Insert(0, currentNode);
+    currentNode = parents.TryGetValue(currentNode, out var parentNode) ? parentNode : null;
+}
+
+Console.WriteLine(string.Join(" ->", pathNodes.Select(i => i.Name)));
+
+Console.WriteLine("Total Distance: {0}", distances[finalNode]);
+
+Console.ReadLine();
